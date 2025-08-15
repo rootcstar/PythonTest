0b5e36170ea95297871a9d838f8348b6bdac337f
demirkadioglu-dot <demirkadioglu@icloud.com>
2025-08-14 20:17:33 -0400

    August 14th
    AddExpense function which gets the description and amount and saves it in a csv file

--- main.py
+++ main.py
@@ -7,9 +7,36 @@ import pandas as pd
 
 
 
+def addExpenses():
+    while True:
+        description = input("Enter the description of the expense: ").strip()
+        if description == "":
+            description = input("No valid description entered")
+            continue
+        break
 
+
+    while True:
+        try:
+            expenseAmount = float(input("Enter expense amount: "))
+            if expenseAmount == 0.0:
+                print("No expense amount provided")
+                continue
+            break
+        except ValueError:
+            print("Invalid amount, please choose a valid number")
+
+    with open("expenses.csv", "a") as f:
+        writer = csv.writer(f)
+        writer.writerow([description, expenseAmount])
+
+    with open("expenses.csv", "r") as f:
+        text = f.readlines()
+        for text in text:
+            print(text)
 
 if __name__ == '__main__':
-    print("Welcome to the Expense Tracker!")
-    print("Demir bunu bugun yaptim")
+    addExpenses()
+
+
 
