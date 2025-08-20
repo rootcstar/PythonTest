3dc773e51f1243599e30399db9423b7fb2041a07
demirkadioglu-dot <demirkadioglu@icloud.com>
2025-08-20 18:49:25 -0400

    August 20th
    Added amount spent and percent by category, fixed the format and user inputs

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def addExpenses():
         dateLength = userDate.split("/")
 
         if len(dateLength) == 3 and dateLength[0].isdigit() and dateLength[1].isdigit() and dateLength[2].isdigit():
-            if len(dateLength[0])== 2 and len(dateLength[1])==2 and len(dateLength[2])==4:
+            if len(dateLength[0])== 2 and len(dateLength[1])==2 and len(dateLength[2])==4 and int(dateLength[0]) <= 12 and int(dateLength[1]) <= 31:
                 break
             else:
                 print("invalid date, enter (MM/DD/YYYY)")
@@ -57,12 +57,11 @@ def addExpenses():
 def viewExpenses():
     with open("expenses.csv", "r") as f:
         reader = csv.reader(f)
-
-        print("="*50)
+        print("=" * 50)
         print("           YOUR EXPENSES")
         print("=" * 50)
         for r in reader:
-            if len(r) <5:
+            if len(r) < 5:
                 continue
             print("Recorded date: " + r[0])
             print("Description  : " + r[1])
@@ -71,6 +70,9 @@ def viewExpenses():
             print("Expense date : " + r[4])
             print("-" * 50)
 
+
+
+
 def spendingAnalysis():
     with open("expenses.csv", "r") as f:
         reader = csv.reader(f)
@@ -78,6 +80,11 @@ def spendingAnalysis():
         lowestExpense = None
         highestRecorded = ""
         lowestRecorded = ""
+        categories = []
+        categorySpent = []
+        totalSpent =0
+
+
 
         for r in reader:
             if len(r) <5:
@@ -89,22 +96,51 @@ def spendingAnalysis():
                 lowestExpense = float(r[2])
                 lowestRecorded = r[0]
 
+            totalSpent += float(r[2])
+            category = r[3].strip().lower()
+            amountSpent = float(r[2])
+            if category not in categories:
+                categories.append(category)
+                categorySpent.append(amountSpent)
+            else:
+                i = categories.index(category)
+                categorySpent[i] += amountSpent
 
 
-
-
+        print("=" * 50)
+        print("           SPENDING ANALYSIS")
+        print("=" * 50)
+        for i in range(len(categories)):
+            percent = int((categorySpent[i]/totalSpent)*100)
+            print(categories[i] + ": $"+ str(categorySpent[i]) + " (%" + str(percent) + ")")
+            print()
+        print("-" * 50)
+        print()
         print("The highest expense is: $" + str(highestExpense) + ", recorded on "+ highestRecorded)
         print("The lowest expense is: $" + str(lowestExpense) + ", recorded on " + lowestRecorded)
 
 
 
-
 if __name__ == '__main__':
-    addExpenses()
-    print()
-    viewExpenses()
-    print()
-    spendingAnalysis()
-    print()
-
+    while True:
+        add = input("Would you like to add a new expense? (Y/N): ")
+        if add == "Y":
+            addExpenses()
+            print()
+        elif add == "N":
+            while True:
+                view = input("Would you like to view the expenses and spending analysis? (Y/N): ")
+                if view == "Y":
+                    viewExpenses()
+                    print("\n")
+                    spendingAnalysis()
+                    print()
+                    break
+                elif view == "N":
+                    print("Thank you for using this program")
+                    exit()
+                else:
+                    print("Invalid input, please try again")
+        else:
+            print("Invalid input, please try again")
 
